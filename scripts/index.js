0c5e03b72d9d36d753c7bff1a20bb8b21729a180
Connor Walters <connorscottwalters@gmail.com>
2022-01-05 02:43:56 -0600

    first-draft

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,117 @@
-console.log('hello world!');
+let acceptIn = false;
+let score = [0, 0];
+game();
 
+let scoreboard = document.getElementById("score");
+let actionText = document.getElementById("actionText");
 
-
-function computerPlay(){
-    let choice = Math.floor(Math.random() * 10) % 3;
-    switch (choice){
-        case 0:
-            return 'Rock';
-        case 1:
-            return 'Paper';
-        default:
-            return 'Scissors';
-    }
+function computerPlay() {
+  let choice = Math.floor(Math.random() * 10) % 3;
+  switch (choice) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    default:
+      return "Scissors";
+  }
 }
 
-function getUserTurn(){
-    //change method of input in future versions
-    return prompt('Enter your move: Rock, Paper, Scissors'); 
+function computeTurn(player, computer) {
+  let PR = "Paper beats rock, ";
+  let SP = "Scissors beats paper, ";
+  let RS = "Rock beats scissors, ";
+  let CW = "computer wins.";
+  let PW = "player wins.";
+
+  let res = "";
+  let score = 0;
+
+  if (player === computer) {
+    res = "Both players played " + player + ", it is a tie.";
+    score--;
+  }
+  if (player === "Rock") {
+    if (computer === "Scissors") {
+      res = RS + PW;
+      score++;
+    }
+    if (computer === "Paper") {
+      res = PR + CW;
+    }
+  }
+
+  if (player === "Paper") {
+    if (computer === "Rock") {
+      res = PR + PW;
+      score++;
+    }
+    if (computer === "Scissors") {
+      res = SP + CW;
+    }
+  }
+
+  if (player === "Scissors") {
+    if (computer === "Rock") {
+      res = RS + CW;
+    }
+    if (computer === "Paper") {
+      res = SP + PW;
+      score++;
+    }
+  }
+  return [res, score];
 }
 
-function computeTurn(player, computer){
-    let PR = 'Paper beats rock, ';
-    let SP = 'Scissors beats paper, ';
-    let RS = 'Rock beats scissors, ';
-    let CW = 'computer wins.';
-    let PW = 'player wins.'
-
-    if (player === computer){
-        return 'Both players played ' + player + ', it is a tie.';
-    }
-    if (player === 'Rock'){
-        if (computer === 'Scissors'){
-            return RS + PW;
-        }
-        if (computer === 'Paper'){
-            return PR + CW;
-        }
-    }
-
-    if (player === 'Paper'){
-        if (computer === 'Rock'){
-            return PR + PW;
-        }
-        if (computer === 'Scissors'){
-            return SP + CW;
-        }
-    }
-
-    if (player === 'Scissors'){
-        if (computer === 'Rock'){
-            return RS + CW;
-        }
-        if (computer === 'Paper'){
-            return SP + PW;
-        }
-    }
+function game() {
+  const page = document.getElementById("page");
+  page.addEventListener("click", sendUserInput);
+  acceptIn = true;
 }
 
-function game(){
-    let log = document.getElementById('log');
-    for (let i = 0; i < 5; i++){
-        tag = document.createElement('p');
-        text = document.createTextNode(computeTurn(getUserTurn(), computerPlay()))
-        tag.appendChild(text);
-        log.appendChild(tag);
-    }
+function sendUserInput(element) {
+  let el = element.target.parentElement.id;
+  if (!["Reset", "Rock", "Paper", "Scissors"].includes(el)) return;
+  let compIn = computerPlay();
+  if (el === "Reset") {
+    resetGame();
+    return;
+  } else if (acceptIn) {
+    updatePic(compIn.toLowerCase(), 1, "computer");
+    updatePic(el.toLowerCase(), 1, "player");
+    let outcome = computeTurn(el, compIn);
+    actionText.innerText = outcome[0];
+    if (outcome[1] === 1) score[0] += 1;
+    else if (outcome[1] === 0) score[1] += 1;
+    updateScoreBoard(score[0], score[1]);
+  }
+
+  if (score[0] >= 5 || score[1] >= 5) {
+    actionText.innerText = score[0] >= 5 ? "Player wins!" : "Computer wins!";
+    acceptIn = false;
+  }
+}
+
+function resetGame() {
+  score[0] = 0;
+  score[1] = 0;
+  updateScoreBoard(score[0], score[1]);
+  acceptIn = true;
+  actionText.innerText = "Choose your attack.";
+  updatePic("", 0, "player");
+  updatePic("", 0, "computer");
+}
+
+function updateScoreBoard(player, comp) {
+  scoreboard.innerText = `${player} - ${comp}`;
+}
+
+function updatePic(choice, visibility, which) {
+  let pic = document.getElementById(which + "Pic");
+  if (choice !== "") pic.src = "./imgs/" + choice + ".png";
+  if (visibility) {
+    pic.style.visibility = "visible";
+  } else {
+    pic.style.visibility = "hidden";
+  }
 }
